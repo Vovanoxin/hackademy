210a53e5bcbe68aac7b5be28f8426a4188fcd264
Volodymyr Tesliuk <vovatesluk6a@gmail.com>
2021-08-07 02:25:46 +0300

    solve ex03

---
+++ courses/cunix/ex03/src/my_strcpy.c
@@ -0,0 +1,14 @@
+char *my_strcpy(char *dest, const char *src)
+{   
+    char *ret = dest;
+    while (*src)
+    {
+        *dest = *src;
+        ++src;
+        ++dest;
+    }
+    *dest = '\0';
+
+    return ret;
+}
+
